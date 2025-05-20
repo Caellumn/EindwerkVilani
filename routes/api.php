1b03d473379a442d5a5de3e91c85efdd71ce5469
Caellumn <Vincent.stoop35@gmail.com>
2025-05-20 16:13:58 +0200

    basic sanctum admin middleware nog te testen

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,16 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\BookingHasServicesController;
 use App\Http\Controllers\BookingHasProductsController;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Http\Middleware\AdminOnly;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//sanctum werkt session based en checked of er een user is ingelogged
+// sanctum stuurt naar de login, maar de error 403 moet je zelf laten sturen ipv een rederict
+//ORIGINAL USER ROUTE
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 // foute manier
 //create apis for products using the product controller
@@ -24,21 +30,28 @@ Route::get('/user', function (Request $request) {
 // Route::put('/products/{product}', [App\Http\Controllers\ProductController::class, 'update']);
 // Route::delete('/products/{product}', [App\Http\Controllers\ProductController::class, 'destroy']);
 
+
+// ---> PUBLIC ROUTES <----------
+//|                              |
+//|                              |
+//|                              |
+//------------------------------
+
 //betere manier
-Route::apiResource('/products', ProductController::class)->only(['index', 'show', 'store', 'destroy','update']);
-Route::apiResource('/services', ServiceApiController::class)->only(['index', 'show', 'store', 'destroy','update']);
+Route::apiResource('/products', ProductController::class)->only(['index', 'show']);
+Route::apiResource('/services', ServiceApiController::class)->only(['index', 'show']);
 
 // Category Routes
-Route::apiResource('/categories', CategoryApiController::class)->only(['index','store']);
+Route::apiResource('/categories', CategoryApiController::class)->only(['index','show']);
 //geeft problemen met de id
 // Route::apiResource('/categories/{categoryId}', CategoryApiController::class)->only(['show','update','destroy']);
 Route::get('/categories/{categoryId}', [CategoryApiController::class, 'show']);
-Route::put('/categories/{categoryId}', [CategoryApiController::class, 'update']);
-Route::delete('/categories/{categoryId}', [CategoryApiController::class, 'destroy']);
+// Route::put('/categories/{categoryId}', [CategoryApiController::class, 'update']);
+// Route::delete('/categories/{categoryId}', [CategoryApiController::class, 'destroy']);
 
 // Product Category Routes
 Route::get('/products/{productId}/categories', [ProductCategoryApiController::class, 'index']);
-Route::put('/products/{productId}/categories/sync', [ProductCategoryApiController::class, 'sync']);
+// Route::put('/products/{productId}/categories/sync', [ProductCategoryApiController::class, 'sync']);
 Route::get('/products-with-categories', [ProductCategoryApiController::class, 'productsWithCategories']);
 Route::get('/product-categories', [ProductCategoryApiController::class, 'productCategories']);
 
@@ -49,21 +62,79 @@ Route::get('/services-with-categories', [ServiceCategoryApiController::class, 's
 Route::get('/service-categories', [ServiceCategoryApiController::class, 'serviceCategories']);
 
 // User Routes
-Route::apiResource('/users', UserController::class)->only(['index', 'show', 'store', 'destroy','update']);
+Route::apiResource('/users', UserController::class)->only(['store']);
 
 // Booking Routes
 Route::apiResource('/bookings', BookingController::class)->only(['index', 'show', 'store', 'destroy','update']);
 
-// Booking Has Products Routes
-Route::get('/bookings/{bookingId}/products', [BookingHasProductsController::class, 'index']);
-Route::get('/bookings-with-products', [BookingHasProductsController::class, 'bookingsWithProducts']);
-Route::put('/bookings/{bookingId}/products/sync', [BookingHasProductsController::class, 'syncProducts']);
-Route::get('/booking-products', [BookingHasProductsController::class, 'bookingProducts']);
+// // Booking Has Products Routes
+// Route::get('/bookings/{bookingId}/products', [BookingHasProductsController::class, 'index']);
+// Route::get('/bookings-with-products', [BookingHasProductsController::class, 'bookingsWithProducts']);
+// Route::put('/bookings/{bookingId}/products/sync', [BookingHasProductsController::class, 'syncProducts']);
+// Route::get('/booking-products', [BookingHasProductsController::class, 'bookingProducts']);
 
-// Booking Has services Routes
-Route::get('/bookings/{bookingId}/services', [BookingHasServicesController::class, 'index']);
-Route::get('/bookings-with-services', [BookingHasServicesController::class, 'bookingsWithServices']);
+// // Booking Has services Routes
+// Route::get('/bookings/{bookingId}/services', [BookingHasServicesController::class, 'index']);
+// Route::get('/bookings-with-services', [BookingHasServicesController::class, 'bookingsWithServices']);
 Route::put('/bookings/{bookingId}/services/sync', [BookingHasServicesController::class, 'syncServices']);
-Route::get('/booking-services', [BookingHasServicesController::class, 'bookingServices']);
+// Route::get('/booking-services', [BookingHasServicesController::class, 'bookingServices']);
 
+// ---> PROTECTED ROUTES <-------
+//|                              |
+//|                              |
+//|                              |
+//-------------------------------
 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    //products routes behind middleware
+    Route::apiResource('/products', ProductController::class)->only(['store', 'destroy','update'])->middleware(AdminOnly::class);
+
+    //services routes behind middleware
+    Route::apiResource('/services', ServiceApiController::class)->only([ 'store', 'destroy','update'])->middleware(AdminOnly::class);
+
+    // users routes behind middleware
+    Route::get('/users', [UserController::class, 'index'])->middleware(AdminOnly::class);
+    Route::apiResource('/users', UserController::class)->except(['index'])->only(['show', 'destroy', 'update'])->middleware(AdminOnly::class);
+
+    //categories routes behind middleware
+    Route::put('/categories/{categoryId}', [CategoryApiController::class, 'update'])->middleware(AdminOnly::class);
+    Route::delete('/categories/{categoryId}', [CategoryApiController::class, 'destroy'])->middleware(AdminOnly::class);
+    Route::apiResource('/categories', CategoryApiController::class)->only(['store'])->middleware(AdminOnly::class);
+
+    //productscategories routes behind middleware
+    Route::put('/products/{productId}/categories/sync', [ProductCategoryApiController::class, 'sync'])->middleware(AdminOnly::class);
+
+    // bookings routes behind middleware
+    Route::apiResource('/bookings', BookingController::class)->only(['index', 'show', 'destroy','update']);
+
+    // Booking Has Products routes behind middleware
+    Route::get('/bookings/{bookingId}/products', [BookingHasProductsController::class, 'index']);
+    Route::get('/bookings-with-products', [BookingHasProductsController::class, 'bookingsWithProducts']);
+    // Route::put('/bookings/{bookingId}/products/sync', [BookingHasProductsController::class, 'syncProducts']);
+    Route::get('/booking-products', [BookingHasProductsController::class, 'bookingProducts']);
+
+    // Booking Has services routes behind middleware
+    Route::get('/bookings/{bookingId}/services', [BookingHasServicesController::class, 'index']);
+    Route::get('/bookings-with-services', [BookingHasServicesController::class, 'bookingsWithServices']);
+    // Route::put('/bookings/{bookingId}/services/sync', [BookingHasServicesController::class, 'syncServices']);
+    Route::get('/booking-services', [BookingHasServicesController::class, 'bookingServices']);
+});
+
+Route::post('/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'role' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    return response()->json(['token' => $token]);
+});
