14747768f36cd3215bcb57183dd76c312f5a34ae
Caellumn <Vincent.stoop35@gmail.com>
2025-05-21 22:22:11 +0200

    trial and error

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyCsrfToken
+{
+
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    
+    protected $except = [
+        'email/verify/*',
+    ];
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($this->isReading($request) || $this->inExceptArray($request)) {
+            return $next($request);
+        }
+
+        // Standard CSRF validation would go here
+        return $next($request);
+    }
+
+    /**
+     * Determine if the request has a URI that should pass through CSRF verification.
+     */
+    protected function inExceptArray(Request $request): bool
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->fullUrlIs($except) || $request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the HTTP request uses a 'read' verb.
+     */
+    protected function isReading(Request $request): bool
+    {
+        return in_array($request->method(), ['HEAD', 'GET', 'OPTIONS']);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminOnly;
+use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\VerifyCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +13,17 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Register global middleware
+        $middleware->prepend(TrustProxies::class);
+        
+        // Web middleware group
+        $middleware->web(append: [
+            VerifyCsrfToken::class,
+        ]);
+        
+        // Existing aliases
         $middleware->alias([
             'admin' => AdminOnly::class
         ]);
